2b23956920c42f4411ec48ece7104372f0716acd
Sudarshan Sreeram <sudarshan.sreeram@arm.com>
2021-08-03 14:26:45 +0100

    Update mock_build with build instructions
    The build instructions were adapted from those defined earlier in
    build.sh. This function has been verfied to work with the supported
    target board and supports building without a mount point.

--- scripts/mock.sh
+++ scripts/mock.sh
@@ -13,7 +13,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+source scripts/utils.sh
+
 # A dummy build function that echos arguments passed to it
 mock_build () {
-  echo "Mock Build:" "$@"
+  toolchain=$1; board=$2; mount=$3; skip=$4; root=$5
+
+  say message "Installing/updating example-specific dependencies..."
+  # 1. Install mbed-os and mbed-os experimental-ble-services (silently)
+  # shellcheck disable=SC2015
+  cd "$root/mock/target" && mbed-tools deploy > /dev/null 2>&1 || \
+    fail exit "Unable to install mbed-os or mbed-os-experimental-ble-services dependency"
+
+  # 2. Install mbed-os python dependencies
+  pip install -q -r mbed-os/requirements.txt || \
+    fail exit "Unable to install mbed-os requirements" "Please take a look at mbed-os/requirements.txt"
+
+  # A short message addressing the known Click dependency conflict - this should be removed once resolved.
+  say note "Click dependency conflict" \
+           "This is a known issue and does not hinder the build process" \
+           "Refer to documentation for more information"
+  say success "Example requirements successfully installed/updated"
+
+  out="cmake_build/$board/develop/$toolchain"
+  # 3. Compile the example with the target board and toolchain
+  # Note: This does not silence errors.
+  mbed-tools compile -t "$toolchain" -m "$board" >/dev/null || \
+    fail exit "Failed to compile the example" "Please check the sources"
+
+  # 4. Convert the output .elf executable to a .bin as it's what NRF52840_DK requires
+  arm-none-eabi-objcopy -O binary "$out/BLE_GattServer_FOTAService.elf" "$out/BLE_GattServer_FOTAService.bin" || \
+    fail exit "Failed to extract binary from elf" "Tip: Check if arm-none-eabi-objcopy is in your path"
+
+  # 5. Flash the board with the binary (if skip is 0)
+  # shellcheck disable=SC2015
+  if [[ "$skip" -eq 0 ]]; then
+    cp "$out/BLE_GattServer_FOTAService.bin" "$mount" || \
+      fail exit "Unable to flash binary!" "Please ensure the board is connected"
+    say success "Binary flashed"
+  else
+    say message "Binary at $root/mock/target/$out/BLE_GattServer_FOTAService.bin"
+  fi
+
+  say success "Build Complete" "Please refer to the documentation for demonstration instructions"
 }
