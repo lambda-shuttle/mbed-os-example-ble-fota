01098f1b35758630ada5a12c1e206aedb129febd
Sudarshan Sreeram <sudarshan.sreeram@arm.com>
2021-08-03 16:08:39 +0100

    Update mcuboot_build routine to install example requirements
    The build instructions for the requirements installation were adapted
    from those defined earlier in build.sh, albeit with minor changes.

--- scripts/mcuboot.sh
+++ scripts/mcuboot.sh
@@ -13,9 +13,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# A dummy build function that echos arguments passed to it
+# Builds the mcuboot example and flashes the binaries if a mount is provided.
+# Please refer to the commented steps for more information
+# Pre: Arguments passed here are all valid
 mcuboot_build () {
-  echo "MCUboot Build:" "$@"
+  toolchain=$1; board=$2; mount=$3; skip=$4; root=$5
+
+  say message "Installing/updating example-specific dependencies..."
+  # 1. Install application dependencies - mbed-os (silently)
+  # shellcheck disable=SC2015
+  cd "$root/mcuboot/target/application" && mbed-tools deploy > /dev/null 2>&1 || \
+    fail exit "Unable to install application dependencies" \
+              "Please check mcuboot.lib, mbed-os.lib, and mbed-os-experimental-ble-services.lib"
+
+  # 2. Install bootloader dependencies (silently)
+  # shellcheck disable=SC2015
+  cd "$root/mcuboot/target/bootloader" && mbed-tools deploy > /dev/null 2>&1 || \
+    fail exit "Unable to install bootloader dependencies" \
+              "Please check mcuboot.lib and mbed-os.lib"
+
+  # 3. Install mbed-os python dependencies (silently)
+  pip install -q -r mbed-os/requirements.txt || \
+    fail exit "Unable to install mbed-os requirements" "Please take a look at mbed-os/requirements.txt"
+
+  # A short message addressing the known Click dependency conflict - this should be removed once resolved.
+  say note "Click dependency conflict" \
+           "This is a known issue and does not hinder the build process" \
+           "Refer to the documentation for more information"
+
+  # 4. Install mcuboot requirements (silently)
+  # shellcheck disable=SC2015
+  cd mcuboot/scripts && pip install -q -r requirements.txt || \
+    fail exit "Unable to install mcuboot requirements" "Please take a look at mcuboot/scripts/requirements.txt"
+
+  # 5. Run mcuboot setup script (silently)
+  python setup.py install > /dev/null 2>&1 || \
+    fail exit "MCUboot setup script failed"
+
+  say success "Example requirements installed/updated"
 }
 
 # A dummy clean function
